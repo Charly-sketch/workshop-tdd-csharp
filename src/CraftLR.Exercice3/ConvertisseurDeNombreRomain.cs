69326a21d47d1c2b8ab25c52717b139a22277bc2
Charly Olivier <145347967+Charly-sketch@users.noreply.github.com>
2024-01-30 10:56:52 +0000

    Exercice3

--- src/CraftLR.Exercice3/ConvertisseurDeNombreRomain.cs
+++ src/CraftLR.Exercice3/ConvertisseurDeNombreRomain.cs
@@ -1,11 +1,75 @@
 using System;
-
+using System.Collections.Generic;
 namespace CraftLR.Exercice3;
 
 public class ConvertisseurDeNombreRomain
 {
-    public int EnNombreArabe(string i)
+    private static readonly Dictionary<char, int> SymbolesRomains = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+    public int EnNombreArabe(string chiffreRomain)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (string.IsNullOrEmpty(chiffreRomain))
+            throw new ArgumentException("Le chiffre romain ne peut pas être vide.");
+
+        if (!EstChiffreRomainValide(chiffreRomain))
+            throw new ArgumentException("Le chiffre romain n'est pas valide.");
+
+        int total = 0;
+
+        for (int i = 0; i < chiffreRomain.Length; i++)
+        {
+            int valeurCourante = SymbolesRomains[chiffreRomain[i]];
+
+            if (i + 1 < chiffreRomain.Length && SymbolesRomains[chiffreRomain[i + 1]] > valeurCourante)
+                total -= valeurCourante;
+            else
+                total += valeurCourante;
+        }
+
+        return total;
+    }
+
+    private bool EstChiffreRomainValide(string chiffreRomain)
+    {
+        for (int i = 0; i < chiffreRomain.Length; i++)
+        {
+            if (!SymbolesRomains.ContainsKey(chiffreRomain[i]))
+                return false;
+
+            if (i + 1 < chiffreRomain.Length && SymbolesRomains[chiffreRomain[i + 1]] > SymbolesRomains[chiffreRomain[i]])
+            {
+                if (!EstSousChiffreRomainValide(chiffreRomain.Substring(i, 2)))
+                    return false;
+
+                i++;
+            }
+        }
+
+        return true;
+    }
+
+    private bool EstSousChiffreRomainValide(string sousChiffreRomain)
+    {
+        // Vérifie la validité des sous-chiffres romains qui peuvent être soustraits (IV, IX, XL, XC, CD, CM)
+        return sousChiffreRomain switch
+        {
+            "IV" => true,
+            "IX" => true,
+            "XL" => true,
+            "XC" => true,
+            "CD" => true,
+            "CM" => true,
+            _ => false
+        };
     }
 }
+
--- tests/CraftLR.Exercice3.Tests/ConvertisseurDeNombreRomainTest.cs
+++ tests/CraftLR.Exercice3.Tests/ConvertisseurDeNombreRomainTest.cs
@@ -11,173 +11,173 @@ public class ConvertisseurDeNombreRomainTest
 {
     private readonly ConvertisseurDeNombreRomain _convertisseurDeNombreRomain = new();
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainIDevraitDonner1()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("I");
         Assert.Equal(1, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainIIDevraitDonner2()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("II");
         Assert.Equal(2, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainIIIDevraitDonner3()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("III");
         Assert.Equal(3, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainVDevraitDonner5()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("V");
         Assert.Equal(5, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainVIDevraitDonner6()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("VI");
         Assert.Equal(6, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainIVDevraitDonner4()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("IV");
         Assert.Equal(4, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainXDevraitDonner10()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("X");
         Assert.Equal(10, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainXIDevraitDonner11()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("XI");
         Assert.Equal(11, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainLDevraitDonner50()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("L");
         Assert.Equal(50, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainXLIXDevraitDonner49()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("XLIX");
         Assert.Equal(49, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainCDevraitDonner100()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("C");
         Assert.Equal(100, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainXCIXDevraitDonner99()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("XCIX");
         Assert.Equal(99, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainCCCXLVIDevraitDonner346()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("CCCXLVI");
         Assert.Equal(346, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainCCCXCIXDevraitDonner399()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("CCCXCIX");
         Assert.Equal(399, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainDDevraitDonner500()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("D");
         Assert.Equal(500, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainMDevraitDonner1000()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("M");
         Assert.Equal(1000, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainMMMMDCCCLXXXVIIIDevraitDonner4888()
     {
         int nombre = _convertisseurDeNombreRomain.EnNombreArabe("MMMMDCCCLXXXVIII");
         Assert.Equal(4888, nombre);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainZDevraitDonnerUneException()
     {
         Assert.Throws<ArgumentException>(() => _convertisseurDeNombreRomain.EnNombreArabe("Z"));
     }
 
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainILDevraitDonnerUneException()
     {
         Assert.Throws<ArgumentException>(() => _convertisseurDeNombreRomain.EnNombreArabe("IL"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
     public void LeChiffreRomainICDevraitDonnerUneException()
     {
         Assert.Throws<ArgumentException>(() => _convertisseurDeNombreRomain.EnNombreArabe("IC"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
 
     public void LeChiffreRomainIDDevraitDonnerUneException()
     {
         Assert.Throws<ArgumentException>(() => _convertisseurDeNombreRomain.EnNombreArabe("ID"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
 
     public void LeChiffreRomainIMDevraitDonnerUneException()
     {
         Assert.Throws<ArgumentException>(() => _convertisseurDeNombreRomain.EnNombreArabe("IM"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
 
     public void LeChiffreRomainVXDevraitDonnerUneException()
     {
         Assert.Throws<ArgumentException>(() => _convertisseurDeNombreRomain.EnNombreArabe("VX"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
 
     public void LeChiffreRomainXDDevraitDonnerUneException()
     {
         Assert.Throws<ArgumentException>(() => _convertisseurDeNombreRomain.EnNombreArabe("XD"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact ]
 
     public void LeChiffreRomainXMDevraitDonnerUneException()
     {
